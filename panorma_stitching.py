9b49dc97b07f4ef8af9fbf6378ce7a3957f16121
netanelhugi <netanelhugi@gmail.com>
2019-10-08 10:50:28 +0300

    update accumulateHomographies function

--- panorma_stitching.py
+++ panorma_stitching.py
@@ -1,51 +1,73 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-def accumulateHomographies(Hpair,m):
-    """
-    Accumulate homography matrix sequence.
-    :param1 Hpair 􀀀 array or dict of M􀀀1 3x3 homography matrices where Hpairfig is a homography that transforms between coordinate systems i and i+1.
-    :param2 m -Index of coordinate system we would like to accumulate the given homographies towards (see details below).
-    """
-    H=[]
-    if len(Hpair)==2: #2 frames
-        H1 =np.matmul(Hpair[1],Hpair[1])
-        H2 =np.identity(3)
-        H3 =np.matmul(np.linalg.inv(Hpair[0]),np.linalg.inv(Hpair[1]))
-        H.append(H1)
-        H.append(H2)
-        H.append(H3)
-        return H
-        # CASE  i < m
-        for i in range(m+1):
-            H_im = Hpair[m]
-            k = m - 1
-            while k >= i:
-                H_im = np.matmul(H_im, Hpair[k])
-                k -= 1
-            # Htemp.append(H_im)
-            H.append(H_im)
-
-        # CASE 2  i = m
-           if i == m:
-              H_im = np.identity(3)
-              H.append(H_im)
-              i += 1
-
-        # CASE 3 i > m
-        for i in range(m + 1, len(Hpair) - 1):
-            k = m + 1
-            H_im = np.linalg.inv(Hpair[m])
-            while k <= i:
-                inverseH = np.linalg.inv(Hpair[k])
-                H_im = np.matmul(H_im, inverseH)
-                k += 1
-            H.append(H_im)
-
-        return H
+'''
+function name: accumulateHomographies
+% ACCUMULATEHOMOGRAPHY Accumulate homography matrix sequence.
+% Arguments:
+% Hpair − array or dict of M−1 3x3 homography matrices where Hpair{i} is a
+% homography that transforms between coordinate systems i and i+1.
+% m − Index of coordinate system we would like to accumulate the
+% given homographies towards (see details below).
+% Returns:
+% Htot − array or dict of M 3x3 homography matrices where Htot{i} transforms
+% coordinate system i to the coordinate system having the index m.
+% Note:
+% In this exercise homography matrices should always maintain
+% the property that H(3,3)==1. This should be done by normalizing them as
+% follows before using them to perform transformations H = H/H(3,3).
+'''
 
 
+def accumulateHomographies(Hpair, m):
+
+    n = len(Hpair)
+    Htot = []
+
+    for mat in range(n+1):
+
+        # CASE 1 -  i < m
+        if mat < m:
+            i = m - 1
+            H_im = Hpair[i]
+
+            while i > mat:
+                i -= 1
+                H_im = np.matmul(H_im, Hpair[i])
+
+            Htot.append(H_im)
+
+        # CASE 2 -  i > m
+        if mat > m:
+            i = m
+            H_im = np.linalg.inv(Hpair[i])
+
+            while i < mat - 1:
+                i += 1
+                # print(i)
+                invH = np.linalg.inv(Hpair[i])
+                H_im = np.matmul(H_im, invH)
+
+            Htot.append(H_im)
+
+        # CASE 3  i = m
+        if mat == m:
+            H_im = np.identity(3)
+            Htot.append(H_im)
+
+    return Htot
 
 
+h1 = [[1, -1, -2], [2, -3, -5], [-1, 3, 5]]
+h2 = [[1, -1, -2], [2, -3, -5], [-1, 3, 5]]
+h3 = [[1, -1, -2], [2, -3, -5], [-1, 3, 5]]
+h4 = [[1, -1, -2], [2, -3, -5], [-1, 3, 5]]
+h5 = [[1, -1, -2], [2, -3, -5], [-1, 3, 5]]
 
+# h = [h1, h2, h3, h4, h5]
+h = [h1, h2]
+
+acc = accumulateHomographies(h, math.ceil(len(h) / 2))
+print(len(acc))
